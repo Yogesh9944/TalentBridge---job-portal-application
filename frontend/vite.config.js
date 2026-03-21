54da1eae34067891f0ec6c85b831ead28715fcfb
Yogesh Sanjay Pande <164549612+Yogesh9944@users.noreply.github.com>
2026-03-21 15:46:10 +0530

    Remove server configuration from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   
-
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: '/',  
 })
